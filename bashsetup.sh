430febd442553236dd0fb564433202876b9de621
Scott Bennett <deadkid@gmail.com>
2025-05-15 12:47:21 -0400

    updates

--- bashsetup.sh
+++ bashsetup.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
-
-
-# https://askubuntu.com/questions/334318/sudoers-file-enable-nopasswd-for-user-all-commands
-# %sudo  ALL=(ALL) NOPASSWD: ALL
-
 sudo apt-get install python3 -y
 
 sudo apt-get install python3-pip -y
