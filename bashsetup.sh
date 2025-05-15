1b3145950865c081fcb23e9f331e02679f508ab1
Scott Bennett <deadkid@gmail.com>
2025-05-15 10:23:22 -0400

    updates

--- bashsetup.sh
+++ bashsetup.sh
@@ -4,20 +4,10 @@
 # https://askubuntu.com/questions/334318/sudoers-file-enable-nopasswd-for-user-all-commands
 # %sudo  ALL=(ALL) NOPASSWD: ALL
 
-touch "$HOME/.hushlogin"
-
 sudo apt-get install python3 -y
 
 sudo apt-get install python3-pip -y
 
 sudo apt-get install ansible -y
-
-git config --global credential.helper "cache --timeout=60480000"
-git config --global core.longpaths true
-git config --global --type bool push.autoSetupRemote true
-git config --global windows.appendAtomically false
-git config --global diff.external difft
-git config --global user.email "deadkid@gmail.com"
-git config --global user.name "Scott Bennett"
 
 ansible-playbook ubuntu_wsl.yml 
