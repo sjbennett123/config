2f2b4f50f0f65cddd88acf8cf23cd3774a7c9c0c
Scott Bennett <deadkid@gmail.com>
2025-05-15 09:01:35 -0400

    updates

--- roles/install_prerequisites/templates/eza.bash
+++ roles/install_prerequisites/templates/eza.bash
@@ -1,10 +1,10 @@
 #!/bin/bash
 if command -v eza > /dev/null ; 
 then
-        alias ls='eza --git --color-scale --hyperlink --group-directories-first'
-        alias ll='eza -l --git --color-scale --hyperlink --group-directories-first'
-        alias lt='eza --git --tree --color-scale --hyperlink --group-directories-first'
-        alias lll='eza --git --extended --long --color-scale --hyperlink --group-directories-first'
+        alias ls='eza --git --color-scale --group-directories-first'
+        alias ll='eza -l --git --color-scale --group-directories-first'
+        alias lt='eza --git --tree --color-scale --group-directories-first'
+        alias lll='eza --git --extended --long --color-scale  --group-directories-first'
 else
         alias ll='ls -l'
 fi
