ebd1ce5bee87b7c8ed039e957890bdd792c76822
Scott Bennett <deadkid@gmail.com>
2025-07-15 16:30:50 -0400

    updates

--- roles/install_prerequisites/templates/git_update.bash
+++ roles/install_prerequisites/templates/git_update.bash
@@ -1,7 +1,10 @@
 #!/bin/bash
-
+set -x
 for dir in */; do
   cd "$dir" || exit ; 
-  git pull -q; 
+  if [[ $(git branch 2> /dev/null)  ]]; then 
+    git fetch --all -q
+    git pull --all -q;
+  fi
   cd ~-  || exit; 
 done
