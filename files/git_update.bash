b5ae205636192a24c8222d9346d5bbf9f51c64f4
Scott Bennett <deadkid@gmail.com>
2025-07-15 16:26:13 -0400

    updates

--- files/git_update.bash
+++ files/git_update.bash
@@ -1,9 +1,10 @@
 #!/bin/bash
+
 for dir in */; do
   cd "$dir" || exit ; 
-  if [[ -n "$(git status -s 2> /dev/null)" ]]; then
-    git fetch --all -q;
-    git pull --all -q; 
+  if [[ -n "$(git status -s 2> /dev/null)" ]]; then 
+    git fetch --all -q
+    git pull --all -q;
   fi
   cd ~-  || exit; 
 done
