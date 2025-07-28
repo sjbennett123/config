869afd8a420a005c1ae5542d1ff0d43ba8673f0c
Scott Bennett <deadkid@gmail.com>
2025-07-28 09:59:50 -0400

    updated Powershell configuration

--- files/Microsoft.PowerShell_profile.ps1
+++ files/Microsoft.PowerShell_profile.ps1
@@ -2,7 +2,7 @@
 # https://learn.microsoft.com/en-us/powershell/scripting/install/installing-powershell-on-windows?view=powershell-7.2
 # https://learn.microsoft.com/en-us/powershell/module/microsoft.powershell.core/about/about_profiles?view=powershell-7.5
 
-# To reload the powershell profile run & $profile
+# To reload the powersasdfhell profile run & $profile
 
 Set-Alias -Name which -Value Get-Command
 Set-Alias -Name cron -Value taskschd.msc
@@ -1518,3 +1518,9 @@ $ocdi_external_services_address
 # curl -X POST --user $uaa_username:$uaa_password= https://$ocdi_external_services_address/uaa/oauth/token -d "grant_type=client_credentials&tenant=mcleodhealth" | jq -c -r '.access_token'
 # token=$(curl -X POST --user ocdiops:BgsrR4fbetswdA9Im6ViSEwX6xs= https://catalyst.3mhis.com/uaa/oauth/token -d "grant_type=client_credentials&tenant=mcleodhealth" | jq -c -r '.access_token ')
 # curl -H "Authorization: Bearer $token" https://ocdi-services-external.cpm-pb.us.amz.3mhis.net/ocdi-services-external/api/diagnosis-code-risk-model-mappings/E11.22
+
+
+if (Test-Path -Path "C:\Users\Public\Desktop\Synergy.lnk" -PathType Leaf) {
+      Write-Output ("To delete the awful shortcut delete the file...")
+      Write-Output ("C:\Users\Public\Desktop\Synergy.lnk")
+}
