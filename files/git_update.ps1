1ccfce59846be461fdba86a251a587bc47ee6351
Scott Bennett <deadkid@gmail.com>
2025-07-29 09:52:30 -0400

    updates

--- files/git_update.ps1
+++ files/git_update.ps1
@@ -1,9 +1,13 @@
 #!/usr/bin/env powershell -File
 $gitdirs = Get-ChildItem -Directory
+
 foreach ($dir in $gitdirs)
 {
   Set-Location $dir
-  git fetch --all
-  git pull --all -q
-  Set-Location ..
+  $is_git_directory = git rev-parse --is-inside-work-tree 2>$null
+  if ($is_git_directory -eq "true") {
+    git fetch --all -q
+    git pull --all  -q
+    Set-Location ..
+  }
 }
