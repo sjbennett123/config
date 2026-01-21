040a84c5faba73aa38f63583e1bd0186d9ebb83a
Scott Bennett <deadkid@gmail.com>
2026-01-21 20:05:10 +0000

    update

--- bashsetup.sh
+++ bashsetup.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
+sudo apt-get update;
+
 sudo apt-get install python3 -y
 
 sudo apt-get install python3-pip -y
 
 sudo apt-get install ansible -y
+
+ansible-galaxy collection install community.general
 
 ansible-playbook ubuntu_wsl.yml 
